8834c29f142f32f2fb90e9fbab21d37a82626d29
Thibault D'Archivio <thibault.darchivio@free.fr>
2012-03-07 22:08:45 +0100

    Création d'une page de déco qui ferme la session ouverte

---
+++ deconnexion.php
@@ -0,0 +1,22 @@
+<?php 
+	
+	session_start();
+	session_destroy();
+	
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+	<head
+		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+		<title>Deconnexion - Tiklr</title>
+	</head>
+	</head>
+	<body>
+		
+		<a href="index.php">Accueil</a>
+		
+	</body>
+</html>
